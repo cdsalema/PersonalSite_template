72a1b452ed2a92dd3d3368cddd446c437b9f68e7
cdsalema <conradod.salema@gmail.com>
2023-02-08 18:58:47 -0300

    JS slidetoggle OK.

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,10 +1,8 @@
-$(function(){
-    $('nav.mobile').click(function(){
-     let listamenu=$('.nav.mobile ul')
-        listamenu.slideToggle()
+$(document).ready(function(){
+    $("nav.mobile").click(function(){
+      $("ul").slideToggle();
     });
-});
-
+  });
     
 
     
