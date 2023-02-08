8b423147292d583c06207b005801ea1c4b7b6600
cdsalema <conradod.salema@gmail.com>
2023-02-08 20:08:26 -0300

    nada de novo só fiz testes q nao deram em nada. rs

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("nav.mobile").click(function(){
-      $("ul").slideToggle();
-    });
-  });
+  $("nav.mobile").click(function(){
+    $("ul").slideToggle();
+  })
+});
     
 
     
