2c1571f51cbf9ba3bd8e51c98adaf42b84d36f68
cdsalema <conradod.salema@gmail.com>
2023-02-08 16:10:24 -0300

    apanhando do Jquery

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,11 +1,10 @@
 $(function(){
     $('nav.mobile').click(function(){
-        var listaMenu = $('nav.mobile ul')
-            if(listaMenu.is(':hidden') === true){
-                listaMenu.fadeIn();
-            }
-            else{
-                listaMenu.fadeOut();
-            }
-    })
-})
+//    var listamenu=$('.nav.mobile ul')
+        listamenu.slideToggle()
+    });
+});
+
+    
+
+    
